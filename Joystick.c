db4990aecd08f7ed71d32a57561b4ff85aafcc99
elmagnificoco <elmagnificogg@gmail.com>
2019-12-30 21:15:26 +0800

    auto start raid at the day

--- Joystick.c
+++ Joystick.c
@@ -41,35 +41,6 @@ static const command step[] = {
 	{ DELAY,      5 },
 	{ DELAY,    250 },
 
-
-	// first change year to 2000
-	{ DOWN,       5 }, // 
-	{ DELAY,      5 },
-	{ RIGHT,     20 }, // select settings
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // enter settings
-	{ DELAY,     50 },
-	{ DOWN,      70 }, // select console
-	{ DELAY,      5 }, // 	
-	{ RIGHT,      5 }, // move to the tab 
-	{ DELAY,      5 },
-	{ DOWN,      20 }, // select date and time 
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // enter change time
-	{ DELAY,     20 },
-	{ DOWN,      20 }, // selec cur time 
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // enter change
-	{ DELAY,     50 },
-	{ DOWN,      50 }, // 2000 
-	{ DELAY,     20 }, // 
-	{ RIGHT,     25 }, // select ok 
-	{ DELAY,     30 },
-	{ A,          5 }, // change
-	{ DELAY,     20 }, // 
-	{ HOME,       5 }, // return home
-	{ DELAY,     50 }, // 
-
 	// enter game
 	{ A,          5 }, // enter game
 	{ DELAY,     50 },
@@ -82,134 +53,6 @@ static const command step[] = {
 	{ A,		  5 }, // game in
 	{ DELAY,	500 }, // 
 
-	// loop wat1
-	{ A,          5 }, // enter cave
-	{ DELAY,     80 },
-	{ A,         10 }, // enter match
-	{ DELAY,    150 }, // 
-	{ HOME,       5 }, // return home
-	{ DELAY,     40 }, // 
-	{ DOWN,       5 }, // 
-	{ DELAY,      5 },
-	{ RIGHT,     20 }, // select settings
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // enter settings
-	{ DELAY,     50 },
-	{ DOWN,      70 }, // select console
-	{ DELAY,      5 }, // 	
-	{ RIGHT,      5 }, // move to the tab 
-	{ DELAY,      5 },
-	{ DOWN,      20 }, // select date and time 
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // enter change time
-	{ DELAY,     20 },
-	{ DOWN,      20 }, // selec cur time 
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // enter change
-	{ DELAY,     50 },
-	{ UP,         5 }, // 2019  
-	{ DELAY,      5 },
-	{ RIGHT,     25 }, // select ok 
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // complete change 
-	{ DELAY,     30 },
-	{ HOME,       5 }, // return home
-	{ DELAY,     40 }, // 
-	{ A,          5 }, // return game
-	{ DELAY,     30 },
-	{ B,          5 }, // quit?
-	{ DELAY,     50 }, // 
-	{ A,          5 }, // yes quit 
-	{ DELAY,    150 },
-
-	// loop wat2
-	{ A,          5 }, // get wat 
-	{ DELAY,     80 },
-	{ A,          5 }, // quick dialogue
-	{ DELAY,     80 },	
-	{ A,          5 }, // got it 
-	{ DELAY,     80 },
-	{ A,         10 }, // enter match
-	{ DELAY,    200 }, // 
-	{ HOME,       5 }, // return home
-	{ DELAY,     40 }, // 
-	{ DOWN,       5 }, // 
-	{ DELAY,      5 },
-	{ RIGHT,     20 }, // select settings
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // enter settings
-	{ DELAY,     50 },
-	{ DOWN,      70 }, // select console
-	{ DELAY,      5 }, // 	
-	{ RIGHT,      5 }, // move to the tab 
-	{ DELAY,      5 },
-	{ DOWN,      20 }, // select date and time 
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // enter change time
-	{ DELAY,     20 },
-	{ DOWN,      20 }, // selec cur time 
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // enter change
-	{ DELAY,     50 },
-	{ UP,         5 }, // 2019  
-	{ DELAY,      5 },
-	{ RIGHT,     25 }, // select ok 
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // complete change 
-	{ DELAY,     30 },
-	{ HOME,       5 }, // return home
-	{ DELAY,     40 }, // 
-	{ A,          5 }, // return game
-	{ DELAY,     30 },
-	{ B,          5 }, // quit?
-	{ DELAY,     50 }, // 
-	{ A,          5 }, // yes quit 
-	{ DELAY,    250 },
-
-	// loop wat3
-	{ A,          5 }, // get wat 
-	{ DELAY,    100 },
-	{ A,          5 }, // quick dialogue
-	{ DELAY,    100 },	
-	{ A,          5 }, // got it 
-	{ DELAY,     80 },
-	{ A,         10 }, // enter match
-	{ DELAY,    200 }, // 
-	{ HOME,       5 }, // return home
-	{ DELAY,     40 }, // 
-	{ DOWN,       5 }, // 
-	{ DELAY,      5 },
-	{ RIGHT,     20 }, // select settings
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // enter settings
-	{ DELAY,     50 },
-	{ DOWN,      70 }, // select console
-	{ DELAY,      5 }, // 	
-	{ RIGHT,      5 }, // move to the tab 
-	{ DELAY,      5 },
-	{ DOWN,      20 }, // select date and time 
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // enter change time
-	{ DELAY,     20 },
-	{ DOWN,      20 }, // selec cur time 
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // enter change
-	{ DELAY,     50 },
-	{ UP,         5 }, // 2019  
-	{ DELAY,      5 },
-	{ RIGHT,     25 }, // select ok 
-	{ DELAY,      5 }, // 
-	{ A,          5 }, // complete change 
-	{ DELAY,     30 },
-	{ HOME,       5 }, // return home
-	{ DELAY,     40 }, // 
-	{ A,          5 }, // return game
-	{ DELAY,     30 },
-	{ B,          5 }, // quit?
-	{ DELAY,     50 }, // 
-	{ A,          5 }, // yes quit 
-	{ DELAY,    300 },
-
 	// share 
 	{ Y,          5 }, // enter Y
 	{ DELAY,     50 },
@@ -220,11 +63,6 @@ static const command step[] = {
 	{ B,		  5 }, // return game
 	{ DELAY,	 50 }, // 
 
-
-	{ A,          5 }, // enter cave
-	{ DELAY,    200 },
-	{ A,          5 }, // get 2000w
-	{ DELAY,    200 },
 	{ A,          5 }, // confirm
 	{ DELAY,    800 },		
 
@@ -245,7 +83,7 @@ static const command step[] = {
 	{ A,		  5 }, // use the password? yes
 	{ DELAY,	300 },
 	{ A,		  5 }, // enter match
-	{ DELAY,	800 },
+	{ DELAY,   1800 },
 	{ UP,         5 }, // 
 	{ DELAY,   3000 },
 	{ A,          5 }, // ready 
@@ -263,9 +101,9 @@ static const command step[] = {
 	
 	{ HOME, 	  5 }, // return home
 	{ DELAY,	 40 }, // 
-	{ X, 	      5 }, // close game
+	{ X, 	      5 }, // close game?
 	{ DELAY,	 30 },
-	{ A,	     20 }, // select settings
+	{ A,	     20 }, // yes
 	{ DELAY,	400 } // 	
 };
 
