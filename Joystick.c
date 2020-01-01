a6ed03c42a767a6c373d6a531c0ebd3834071a58
elmagnificoco <elmagnificogg@gmail.com>
2020-01-02 01:03:14 +0800

    very fast get wat

--- Joystick.c
+++ Joystick.c
@@ -31,68 +31,51 @@ typedef struct {
 
 static const command step[] = {
 	// Setup controller
-	{ DELAY,    250 },
-	{ TRIGGERS,      5 },
+	{ DELAY,    300 },
+	{ TRIGGERS,   5 },
 	{ DELAY,    150 },
-	{ TRIGGERS,      5 },
+	{ TRIGGERS,   5 },
 	{ DELAY,    150 },
-	{ A,      5 },
-	{ DELAY,    250 },
+	{ DELAY,      5 },
+	{ DELAY,      5 },
 
 	// loop wat
-	{ A,          5 }, // enter cave
-	{ DELAY,     30 },
-	{ A,         10 }, // enter match
-	{ DELAY,    120 }, // 
 	{ HOME,       5 }, // return home
-	{ DELAY,     40 }, // 
+	{ DELAY,     20}, // 
 	{ DOWN,       5 }, // 
-	{ DELAY,      5 },
 	{ RIGHT,     20 }, // select settings
-	{ DELAY,      5 }, // 
 	{ A,          5 }, // enter settings
-	{ DELAY,     50 },
+	{ DELAY,     10 },
 	{ DOWN,      70 }, // select console
-	{ DELAY,      5 }, // 	
 	{ RIGHT,      5 }, // move to the tab 
-	{ DELAY,      5 },
-	{ DOWN,      20 }, // select date and time 
-	{ DELAY,      5 }, // 
+	{ DOWN,      20 }, // select date and time  
 	{ A,          5 }, // enter change time
-	{ DELAY,     20 },
-	{ DOWN,      20 }, // selec cur time 
-	{ DELAY,      5 }, // 
+	{ DELAY,     15 },
+	{ DOWN,      15 }, // selec cur time 
 	{ A,          5 }, // enter change
-	{ DELAY,     50 },
-	//{ DOWN,       5 }, // 2018 
-	//{ DELAY,     20 }, // 
-	//{ RIGHT,     25 }, // select ok 
-	//{ DELAY,     30 },
-	//{ A,          5 }, // change
-	//{ DELAY,     20 }, // 
-	//{ A,          5 }, // enter change again
-	//{ DELAY,     30 },
-	//{ LEFT,      25 }, // select year 
-	//{ DELAY,     20 }, // 
-	{ UP,         5 }, // 2019  
-	{ DELAY,      5 },
+	{ DELAY,     10 },
+	{ DOWN,       5 }, // 2018 
 	{ RIGHT,     25 }, // select ok 
+	{ A,          5 }, // change
 	{ DELAY,      5 }, // 
+	{ A,          5 }, // enter change again
+	{ DELAY,      3 },
+	{ LEFT,      25 }, // select year 
+	{ UP,         5 }, // 2019  
+	{ RIGHT,     25 }, // select ok 
 	{ A,          5 }, // complete change 
-	{ DELAY,     30 },
+	{ DELAY,      5 },
 	{ HOME,       5 }, // return home
 	{ DELAY,     20 }, // 
 	{ A,          5 }, // return game
 	{ DELAY,     30 },
-	{ B,          5 }, // quit?
-	{ DELAY,     50 }, // 
-	{ A,          5 }, // yes quit 
-	{ DELAY,    100 },
 	{ A,          5 }, // get wat 
 	{ DELAY,     30 },
 	{ A,          5 }, // quick dialogue
 	{ DELAY,     50 },	
 	{ A,          5 }, // got it 
+	{ DELAY,     50 },
+	{ B,          5 }, // quit?
 	{ DELAY,     50 }
 	
 };
@@ -306,7 +289,7 @@ void GetNextReport(USB_JoystickReport_Input_t* const ReportData) {
 
 		case PROCESS:
 
-			switch (step[bufindex].button)
+			switch (step[bufindex].opt)
 			{
 
 				case UP:
